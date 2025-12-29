b078d1acc3689438f1b092faeaf4b8eacc12234b
faturrahman10 <faturrahmanhasir@gmail.com>
2025-12-29 22:14:29 +0800

    add new field to migration and model category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'is_active'];
 
 public function menus(){
         return $this->hasMany(Menu::class);
