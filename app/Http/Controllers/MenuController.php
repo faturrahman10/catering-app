3bebfb587c4933f2711009b5cdb1399b90a96d42
faturrahman10 <faturrahmanhasir@gmail.com>
2025-12-30 06:05:12 +0800

    create menuController and all mehod for menu

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Menu;
+use App\Models\Category;
+
+class MenuController extends Controller
+{
+    public function index()
+    {
+        $menu = Menu::with('category')->latest()->get();
+        return view('menus.index', compact('menu'));
+    }
+
+    public function create()
+    {
+        $categories = Category::all();
+        return view('menus.create', compact('categories'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'category_id' => 'required',
+            'price' => 'required|numeric',
+            'image' => 'required|image',
+            'description' => 'nullable|string',
+        ]);
+
+        $data = $request->all();
+
+        if ($request->hasFile('image')) {
+            $data['image'] = $request->file('image')->stores('menus', 'public');
+        }
+
+        $data['is_active'] = $request->has('is_active');
+
+        Menu::create($data);
+
+        return redirect()->route('menus.index');
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(Menu $menu)
+    {
+        $categories = Category::all();
+        return view('menus.edit', compact('manu', 'categories'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'category_id' => 'required',
+            'price' => 'required|numeric',
+            'image' => 'required|image',
+            'description' => 'required|string',
+        ]);
+
+        $data = $request->all();
+
+        if ($request->hasFile('image')) {
+            $data['image'] = $request->file('image')->store('menus', 'public');
+        }
+
+        $data['is_active'] = $request->has('is_active');
+
+        Menu::update($data);
+
+        return redirect()->route('menus.index');
+    }
+
+    public function destroy(Menu $menu)
+    {
+        $menu->delete();
+        return redirect()->route('menus.index');
+    }
+}
