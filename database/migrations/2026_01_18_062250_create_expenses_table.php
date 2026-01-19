383ec56295b2537bb2c035ad6269e941e2ad4faa
faturrahman10 <faturrahmanhasir@gmail.com>
2026-01-19 19:55:47 +0800

    create migration for expenses and income

---
+++ database/migrations/2026_01_18_062250_create_expenses_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->text('description');
+            $table->integer('amount');
+            $table->date('expense_date');
+            $table->string('receipt_image')->nullable();
+            $table->text('notes')->nullable();
+            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
