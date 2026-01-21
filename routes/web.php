41b620c43855ae75048a3c1e97ec9220c80d2bee
faturrahman10 <faturrahmanhasir@gmail.com>
2026-01-22 07:07:11 +0800

    add all route for keuangan feature

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\FinanceController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\IncomeController;
 
 Route::get('/', [LandingPageController::class, 'index'])->name('home');
 
@@ -25,6 +28,28 @@ Route::middleware(['auth', 'admin', 'verified'])->group(function () {
     Route::patch('/orders/{order}/status', [OrderController::class, 'updateStatus'])->name('orders.updateStatus');
     Route::get('/customers/search', [CustomerController::class, 'search'])->name('customers.search');
     Route::resource('customers', CustomerController::class);
+
+    // Finance Routes
+    Route::get('/finance', [FinanceController::class, 'index'])->name('finance.index');
+    Route::get('/finance/sales', [FinanceController::class, 'sales'])->name('finance.sales');
+    Route::resource('finance/expenses', ExpenseController::class)->names([
+        'index' => 'finance.expenses.index',
+        'create' => 'finance.expenses.create',
+        'store' => 'finance.expenses.store',
+        'show' => 'finance.expenses.show',
+        'edit' => 'finance.expenses.edit',
+        'update' => 'finance.expenses.update',
+        'destroy' => 'finance.expenses.destroy',
+    ]);
+    Route::resource('finance/incomes', IncomeController::class)->names([
+        'index' => 'finance.incomes.index',
+        'create' => 'finance.incomes.create',
+        'store' => 'finance.incomes.store',
+        'show' => 'finance.incomes.show',
+        'edit' => 'finance.incomes.edit',
+        'update' => 'finance.incomes.update',
+        'destroy' => 'finance.incomes.destroy',
+    ]);
 });
 
 require __DIR__ . '/auth.php';
