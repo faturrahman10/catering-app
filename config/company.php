e0c2e45436b56894330c0f656c78554e816dd8e3
faturrahman10 <faturrahmanhasir@gmail.com>
2026-01-24 14:10:44 +0800

    add config files for global Excel and PDF export rules

---
+++ config/company.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => env('COMPANY_NAME', 'Catering Business'),
+    'address' => env('COMPANY_ADDRESS', 'Makassar, South Sulawesi, Indonesia'),
+    'phone' => env('COMPANY_PHONE', '0812-xxxx-xxxx'),
+    'email' => env('COMPANY_EMAIL', 'info@catering.com'),
+    'logo' => env('COMPANY_LOGO', 'images/company/logo.png'),
+];
