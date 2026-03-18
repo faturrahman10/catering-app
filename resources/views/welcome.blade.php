93c3e5e6e3637e3f2a939c29edb317794a0d154e
faturrahman10 <faturrahmanhasir@gmail.com>
2026-03-18 22:21:29 +0800

    change text content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2524,7 +2524,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M9 5l7 7-7 7" />
                                 </svg>
-                                <span>Kebijakan Privasi</span>
+                                <span>Privasi</span>
                             </a>
                         </li>
                         <li>
